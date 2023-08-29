923fe4b702fb86187eab3b92074ce9cabfb6a46b
joerow <joerow@gmail.com>
2023-08-29 21:44:42 +0800

    add create country options from script

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("script linked");
+
+const countries = [
+  {
+    countryName: "Great Britain",
+    postcodeFormat: /[a-z]\d\d\s\d[a-z][a-z]/,
+  },
+  {
+    countryName: "United States",
+    postcodeFormat: /\d\d\d\d\d/,
+  },
+];
+
+let selectOptions = document.getElementById("userCountry");
+console.log(selectOptions);
+countries.forEach((country) => {
+  console.log(country.countryName);
+  let option = document.createElement("option");
+  option.value = country.countryName;
+  option.innerText = country.countryName;
+  selectOptions.appendChild(option);
+  console.log("this ran");
+});
