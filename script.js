0b26392a8141e2f33360e2a4a8271455575c528e
joerow <joerow@gmail.com>
2023-09-04 21:18:30 +0800

    add all form fields and some validation

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const countries = [
   {
     countryName: "Great Britain",
     postcodeFormat: /[a-z]\d\d\s\d[a-z][a-z]/,
+    postcodeFormatPlain: "S43 3NF",
   },
   {
     countryName: "United States",
     postcodeFormat: /\d\d\d\d\d/,
+    postcodeFormatPlain: "57044",
   },
 ];
 
@@ -21,3 +23,33 @@ countries.forEach((country) => {
   selectOptions.appendChild(option);
   console.log("this ran");
 });
+
+const emailInput = document.getElementById("userEmail");
+console.log(emailInput);
+const emailError = document.querySelector("#userEmail + span.error");
+console.log(emailError);
+emailInput.addEventListener("input", (event) => {
+  // Each time the user types something, we check if the
+  // form fields are valid.
+  if (emailInput.validity.valid) {
+    // In case there is an error message visible, if the field
+    // is valid, we remove the error message.
+    emailError.textContent = ""; // Reset the content of the message
+    emailError.className = "error"; // Reset the visual state of the message
+  } else {
+    // If there is still an error, show the correct error
+    showError("email");
+  }
+});
+
+function showError(error) {
+  if (error == "email") {
+    if (emailInput.validity.patternMismatch) {
+      // If the field is empty,
+      // display the following error message.
+      emailError.textContent = "please enter a valid email";
+    }
+    // Set the styling appropriately
+    emailError.className = "error active";
+  }
+}
